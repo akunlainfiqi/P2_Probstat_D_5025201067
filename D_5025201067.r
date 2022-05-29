338fd799b3141d43a5b64fe3b4f3b02aa5611398
akunlainfiqi <fiqi296@gmail.com>
2022-05-30 00:01:47 +0700

    nomer 2 komplit

--- D_5025201067.r
+++ D_5025201067.r
@@ -1,4 +1,5 @@
 install.packages("BSDA")
+library(BSDA)
 #1
 x <- c(78,75,67,77,70,72,28,74,77)
 y <- c(100,95,70,90,90,90,89,90,100)
@@ -18,3 +19,8 @@ t.test(x,y,mu=0,var.equal=TRUE)
 
 #2B
 tsum.test(mean.x=23500,s.x=3900,n.x=100,mu=20000,alternative="greater",var.equal=TRUE)
+
+#2C
+#kesimpulan yang didapatkan adalah rata-rata mobil lebih dari 20rb km/tahun
+
+#3
