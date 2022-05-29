68b3f01467306f1083566d1f3db96184f045ecbe
akunlainfiqi <fiqi296@gmail.com>
2022-05-30 00:00:48 +0700

    nomer 2

--- D_5025201067.r
+++ D_5025201067.r
@@ -1,3 +1,4 @@
+install.packages("BSDA")
 #1
 x <- c(78,75,67,77,70,72,28,74,77)
 y <- c(100,95,70,90,90,90,89,90,100)
@@ -7,4 +8,13 @@ sdev <- sd(selisih)
 sdev
 
 #1B
+t.test(x,y,paired=TRUE)
 
+#1C
+t.test(x,y,mu=0,var.equal=TRUE)
+
+#2A
+#setuju
+
+#2B
+tsum.test(mean.x=23500,s.x=3900,n.x=100,mu=20000,alternative="greater",var.equal=TRUE)
